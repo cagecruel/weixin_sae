bbf0e700b1928f213caa7f7f62df94eaae53225e
cruelcage <cruelcage@gmail.com>
2013-09-11 11:31:02 +0800

    at work init

--- weixin/lxml_test.py
+++ weixin/lxml_test.py
@@ -1,4 +1,5 @@
 # coding:UTF-8
+
 import hashlib
 import web
 import lxml
@@ -11,27 +12,31 @@ import xml.etree.ElementTree as ET
 
 import sys
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding('utf-8') 
 
-name = ["名称	   %s\n",
-        "时间    %s\n",
-        "最新价%s\n",
-        "昨收盘  %s\n",
-        "今开盘  %s\n",
-        "涨跌额  %s\n",
-        "最低    %s\n",
-        "最高    %s\n",
-        "涨跌幅  %s\n",
-        "成交量  %s手\n",
-        "成交额  %s万"
-        ]
+PREFIX = ["股票代码", #unused,just take place
+          "名称	    %s\n",
+          "时间      %s\n",
+          "最新价    %s\n",
+          "昨收盘    %s\n",
+          "今开盘    %s\n",
+          "涨跌额    %s\n",
+          "最低       %s\n",
+          "最高       %s\n",
+          "涨跌幅    %s\n",
+          "成交量    %s手\n",
+          "成交额    %s万"
+         ] 
+
+URL_NAME = {"getInfo" : "http://www.webxml.com.cn/WebServices/ChinaStockWebService.asmx/getStockInfoByCode?theStockCode",
+            "getKGraph" : ""}
 
 app_root = os.path.dirname(__file__)
 templates_root = os.path.join(app_root, 'templates')
 render = web.template.render(templates_root)
 
 class WeixinInterface:    
-       
+        
     def GET(self):
         # 获取输入参数
         data = web.input()
@@ -52,7 +57,7 @@ class WeixinInterface:
         if hashcode == signature:
             # print "true"
             return echostr
-       
+        
     def POST(self):
         # 从获取的xml构造xml dom树
         str_xml = web.data()
@@ -61,18 +66,26 @@ class WeixinInterface:
         fromUser = xml.find("FromUserName").text            
         toUser = xml.find("ToUserName").text
         msgType = xml.find("MsgType").text
-       
+        
         content = xml.find("Content").text
         # query with the stock number
-        url = urllib.urlopen("http://www.webxml.com.cn/WebServices/ChinaStockWebService.asmx/getStockInfoByCode?theStockCode=%s"%content)
+        url = urllib.urlopen(URL_NAME["getInfo"] + "=%s"%content)
         tree = ET.parse(url)
         root = tree.getroot()
                
         # stock name
         content = ""
         #content = name[0]%(root[0].text)
-        for i in range(1, len(name) + 1):
-            content += name[i-1]%(root[i].text)
-       
+        for i in range(1, len(PREFIX)):
+            # can replace '2' with some readable name ,(e.g.,"stocktime"
+            if i == 2:
+                # hide the time field, string.split() not work
+                content += PREFIX[i]%(root[2].text[11:])
+                pass
+                #time = "qadsd"
+                #root[i].text.split(' ')[1]
+            else:
+                # add node start from the second,the first node is the stock number(sh601919)            
+            	content += PREFIX[i]%(root[i].text)
         # 模板渲染
         return render.reply_text(fromUser, toUser, int(time.time()), msgType, content)
